0b27669ae9a16294d40455c81abcf7baf2e5878e
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2019-10-11 02:29:50 +0300

    add extensions

--- DailyMenu/Source/Extensions/Color.swift
+++ DailyMenu/Source/Extensions/Color.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2019 epam. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    class var d_commonBlue: UIColor {
+        return UIColor(red: 61.0 / 255.0, green: 153.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+}
--- DailyMenu/Source/Extensions/UIButton.swift
+++ DailyMenu/Source/Extensions/UIButton.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIButton {
+    private func actionHandler(action:(() -> Void)? = nil) {
+        struct __ { static var action :(() -> Void)? }
+        if action != nil { __.action = action }
+        else { __.action?() }
+    }
+    @objc private func triggerActionHandler() {
+        self.actionHandler()
+    }
+    func actionHandler(controlEvents control :UIControl.Event, ForAction action:@escaping () -> Void) {
+        actionHandler(action: action)
+        removeTarget(self, action: nil, for: .allEvents)
+        addTarget(self, action: #selector(triggerActionHandler), for: control)
+    }
+}
