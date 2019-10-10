de859a1f0cb63767b453f3dcf01c62997cc24629
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2019-10-11 02:30:00 +0300

    update delegate

--- DailyMenu/Source/AppDelegate.swift
+++ DailyMenu/Source/AppDelegate.swift
@@ -13,30 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var exploreTab: UINavigationController {
-        let vm = RestaurantsViewModelImplementation()
-        let vc = RestaurantsViewController(viewModel: vm)
-        vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Explore-Inactive")
-        vc.tabBarItem.title = "Explore"
-        return UINavigationController(rootViewController: vc)
-    }
-    
-    var settingsTab: UINavigationController {
-        let vm = SettingsViewModelImplementation()
-        let vc = SettingsViewController(viewModel: vm)
-        vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Settings-Inactive")
-        vc.tabBarItem.title = "Settings"
-        return UINavigationController(rootViewController: vc)
-    }
-    
-    var cartTab: UINavigationController {
-        let vm = CartViewModelImplementation()
-        let vc = CartViewController(viewModel: vm)
-        vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Cart-Inactive")
-        vc.tabBarItem.title = "Cart"
-        return UINavigationController(rootViewController: vc)
-    }
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let tabbar = UITabBarController()
@@ -50,8 +26,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window!.rootViewController = tabbar
         self.window!.makeKeyAndVisible()
         
+        tabbar.show(GreetingViewController(viewModel: GreetingViewModelImplementation()), sender: nil)
+        
         return true
     }
     
+}
+
+private extension AppDelegate {
+    var exploreTab: UINavigationController {
+          let vm = RestaurantsViewModelImplementation()
+          let vc = RestaurantsViewController(viewModel: vm)
+          vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Explore-Inactive")
+          vc.tabBarItem.title = "Explore"
+          return UINavigationController(rootViewController: vc)
+      }
+      
+      var settingsTab: UINavigationController {
+          let vm = SettingsViewModelImplementation()
+          let vc = SettingsViewController(viewModel: vm)
+          vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Settings-Inactive")
+          vc.tabBarItem.title = "Settings"
+          return UINavigationController(rootViewController: vc)
+      }
+      
+      var cartTab: UINavigationController {
+          let vm = CartViewModelImplementation()
+          let vc = CartViewController(viewModel: vm)
+          vc.tabBarItem.image = #imageLiteral(resourceName: "Tab-Cart-Inactive")
+          vc.tabBarItem.title = "Cart"
+          return UINavigationController(rootViewController: vc)
+      }
 }
 
