f18261847aa3d734870a9bf004aa10da575d658d
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2019-10-27 21:37:42 +0300

    add tablekit to restaurants view controller

--- DailyMenu/Source/Screens/Restaurants/RestaurantsViewController.swift
+++ DailyMenu/Source/Screens/Restaurants/RestaurantsViewController.swift
@@ -5,10 +5,15 @@
 
 
 import UIKit
+import TableKit
+import SnapKit
 
 final class RestaurantsViewController: UIViewController {
 
   private var viewModel: RestaurantsViewModel
+  
+  private var tableView: UITableView!
+  private var tableDirector: TableDirector!
 
   init(viewModel: RestaurantsViewModel) {
     self.viewModel = viewModel
@@ -20,10 +25,27 @@ final class RestaurantsViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
     view.backgroundColor = .white
     
     viewModel.view = self
+  
+    tableView = UITableView()
+    tableView.backgroundColor = Colors.lightGray.color
+    
+    self.view.addSubview(tableView)
+    tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    
+    tableDirector = TableDirector(tableView: tableView, shouldUsePrototypeCellHeightCalculation: true)
+    
+    let rows = viewModel.restaurants.map { makeRowForRestaurant(restaurant: $0) }
+   
+    tableDirector.append(rows: rows)
+    tableDirector.reload()
+  }
+  
+  func makeRowForRestaurant(restaurant: Restaurant) -> TableRow<RestaurantCell>{
+    let row = TableRow<RestaurantCell>(item: RestaurantCellItem(name: restaurant.alias, rate: restaurant.rate, deliveryFee: "2.00"))
+    return row
   }
 
 }
