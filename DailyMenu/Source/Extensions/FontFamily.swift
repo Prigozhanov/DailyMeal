e772c38532bd7f7b1a8fa80779251baddf3e3138
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2019-10-27 22:36:53 +0300

    add font family extension

---
+++ DailyMenu/Source/Extensions/FontFamily.swift
@@ -0,0 +1,17 @@
+//
+//  FontFamily.swift
+//  DailyMenu
+//
+//  Created by Vladimir on 10/27/19.
+//  Copyright © 2019 epam. All rights reserved.
+//
+
+import UIKit
+
+extension FontFamily {
+  
+  public static var hugeBoldFont: UIFont? {
+    return Poppins.bold.font(size: 35)
+  }
+  
+}
